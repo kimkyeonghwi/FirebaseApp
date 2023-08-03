716868addff5b47a7457a3cab4e5d4278446a023
kimkyeonghwi <ppjs2004@naver.com>
2023-08-03 14:53:26 +0900

    구글 연동

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import {GoogleSignin, GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
-const App: React.FC = () => {
+const googleSigninConfigure = () => {
+  GoogleSignin.configure({
+    webClientId: '192091912437-vthneedn5hkfosh3cur0df3fvsgjhcca.apps.googleusercontent.com',
+  });
+};
+
+const onGoogleButtonPress = async () => {
+  const {idToken} = await GoogleSignin.signIn();
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  return auth().signInWithCredential(googleCredential);
+};
+
+const App = () => {
+  useEffect(() => {
+    googleSigninConfigure();
+  });
   return (
     <SafeAreaView>
+      <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
       <Text>asd</Text>
     </SafeAreaView>
   );
